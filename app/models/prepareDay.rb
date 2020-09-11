90d202026aec54ce6dbaa0e91fbf09f1be3a6c02
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-09-12 01:59:08 +0900

    削除済みファイル

--- app/models/prepareDay.rb
+++
@@ -1,6 +0,0 @@
-class PrepareDay < ActiveHash::Base
-  # self.data = [
-  #   {id: 1, prepare_day: '1~2日で発送'}, {id: 2, prepare_day: '2~3日で発送'}, {id: 3, prepare_day: '4~7日で発送'}
-
-  # ]
-end
--- db/migrate/20200906105118_rename_prefecture_code_column_to_items.rb
+++
@@ -1,8 +0,0 @@
-class RenamePrefectureCodeColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :prefecture_code, :prefecture_id
-    rename_column :items, :prepare_day, :prepare_day_id
-    rename_column :items, :delivery_burden, :delivery_burden_id
-    rename_column :items, :condition, :condition_id
-  end
-end
