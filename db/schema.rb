a1f1ff184286e00d200d8be6a500fea052da9091
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-09-12 02:01:10 +0900

    マイグレーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_235821) do
+ActiveRecord::Schema.define(version: 2020_09_06_064159) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_235821) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "category_id", null: false
