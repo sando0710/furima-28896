375c7148c80f499e6606fccf322cfb707742a267
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-09-12 12:24:37 +0900

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_235821) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "category_id", null: false
