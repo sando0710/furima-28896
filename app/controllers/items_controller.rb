65f4708e3e5bc30b295ba83f80310dac57d4518b
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-09-12 22:12:01 +0900

    itemsコントローラの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,13 @@ class ItemsController < ApplicationController
         render :edit
       end
   end 
-
-
+  
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path 
+    end
+  end
 
   private
 
