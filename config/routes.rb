38a02bae2906d176e5b410cd0b34114650c3188b
sando0710 <54323405+sando0710@users.noreply.github.com>
2020-09-13 20:35:41 +0900

    recovery.all

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'items#index'
-
-  resources :items
   
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items do
+    resources :exchanges, only: [:index, :new, :create]
+  end
 end
